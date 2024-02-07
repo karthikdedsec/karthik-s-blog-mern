f162d5597dc71d06f23899ad0813f33774d2dd38
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-02-07 11:58:04 +0530

    create the delete user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -55,3 +55,17 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+//delete user
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.deleteId) {
+    return next(errorHandler(404, "Could not delete user"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.deleteId);
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
