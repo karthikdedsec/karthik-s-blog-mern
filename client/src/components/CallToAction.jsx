2f96a7bb9e79be62c9d1694a061b09692c3fd632
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-02-12 17:27:18 +0530

    add call to action to the post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,30 @@
+import { Button } from "flowbite-react";
+import { Link } from "react-router-dom";
+
+function CallToAction() {
+  return (
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+      <div className="flex-1 justify-center flex flex-col">
+        <h2 className="text-2xl">
+          Ready to take your skills to the next level?
+        </h2>
+        <p className="text-gray-500 my-2">checkout our website</p>
+        <Button
+          className="rounded-tl-xl rounded-bl-none rounded-tr-none"
+          gradientDuoTone="greenToBlue"
+        >
+          <Link rel="noopener noreferrer" target="_blank" to="#">
+            Join Us
+          </Link>
+        </Button>
+      </div>
+      <div className="p-7 flex-1">
+        <img
+          src="https://cdn.sanity.io/images/tlr8oxjg/production/222b99d8aec30b1277f7b760c66bba9e707cd81a-1456x816.png?w=3840&q=80&fit=clip&auto=format"
+          alt="image"
+        />
+      </div>
+    </div>
+  );
+}
+export default CallToAction;
